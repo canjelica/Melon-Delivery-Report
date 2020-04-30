129df2d87cae5730c1e5c370fceb1ff24c688aad
Cassandra Sutton <cassandrasutton@gmail.com>
2020-04-30 00:26:36 -0700

    Created function to produce melon type, count sold, and amount sold in dollars

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,44 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def produce_melon_count_sales_by_day():
+    
+    print("Day 1")
+    the_file = open("um-deliveries-20140519.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        print("Delivered {} {}s for total of ${}".format(count, melon, amount))
+    the_file.close()
 
+    print("Day 2")
+    the_file = open("um-deliveries-20140520.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+    print("Day 3")
+    the_file = open("um-deliveries-20140521.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
+produce_melon_count_sales_by_day()
