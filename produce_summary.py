7a5c1fceffa6f1a0696442925f6d224eaad96d38
Cassandra Sutton <cassandrasutton@gmail.com>
2020-04-30 00:33:08 -0700

    added comments by line explaining each section of code

--- produce_summary.py
+++ produce_summary.py
@@ -4,16 +4,16 @@ def produce_melon_count_sales_by_day():
     Looks at daily sales logs and creates report of the number, type of melon, and total amount sold"""  
     
     print("Day 1")
-    the_file = open("um-deliveries-20140519.txt")
-    for line in the_file:
+    the_file = open("um-deliveries-20140519.txt") #this opens daily sales log
+    for line in the_file:       #iterates over each line in opened saily sales log, tokenizes each item by delimitor"
         line = line.rstrip()
         words = line.split('|')
 
-        melon = words[0]
+        melon = words[0]        #saves each item from tokenized file to variable by index
         count = words[1]
         amount = words[2]
 
-        print("Delivered {} {}s for total of ${}".format(count, melon, amount))
+        print("Delivered {} {}s for total of ${}".format(count, melon, amount)) #prints the number of type of melons and amount sold
     the_file.close()
 
     print("Day 2")
